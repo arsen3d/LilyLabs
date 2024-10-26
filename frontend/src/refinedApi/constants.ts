7bbb9b544d97364a1036d4da07dda89c79090501
arsen3d <arsen3d@gmail.com>
2024-10-26 03:51:48 +0000

    domain update

--- frontend/src/refinedApi/constants.ts
+++ frontend/src/refinedApi/constants.ts
@@ -1,5 +1,5 @@
 const apiConstants = {
-  hostname: `${window.location.protocol}//${window.location.hostname}:8000`
+  hostname:  "https://lilylabs.onrender.com"//`${window.location.protocol}//${window.location.hostname}:8000`
 }
 
 export default apiConstants;
